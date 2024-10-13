84d9c33a5dc113d1c65d945f872e58b5b9e67230
abdoul-nasser <abdoulnassermoh7@gmail.com>
2024-10-13 02:32:49 +0100

    initialisation de mon projet d'application full stacks

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (_, res) => {
+  res.send('Hello expression')
+})
 
 app.listen(port, () => {
   console.log(`Serveur demarrer avec succes 4000`)
